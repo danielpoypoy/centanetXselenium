2feca0eaca59a2efbfbbed7795800e4cc2abf17f
daniel <skyboy0508@gmail.com>
2023-03-01 00:55:25 +0800

    use div[0] method

--- centranet_data.py
+++ centranet_data.py
@@ -1,73 +1,63 @@
+import time
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
-import datetime as dt
+from selenium.webdriver.common.by import By
 import pandas as pd
-
-#open chrome web browser
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get("https://hk.centanet.com/findproperty/en/list/transaction")
-
 def capitalize(words):
+    words= words.split()
     for i in range(len(words)):
         words[i] = words[i].capitalize()
     words = ' '.join(words)
     return words
-
-#Input address and select sold/lease
 wait = WebDriverWait(driver, 10)
 inputElem = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "input")))
 inputElem.clear()
 print('Enter property name:')
 propertys = input()
-capitalize(propertys)
+propertys = capitalize(propertys)
 print('Enter Sold/Leased:')
 sold_leased = input()
-capitalize(sold_leased)
+sold_leased = capitalize(sold_leased)
 inputElem.send_keys(propertys)
 wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "btn-search"))).click()
 wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@class='btn-fiter']/span[contains(text(), 'Sold / Leased')]"))).click()
 wait.until(EC.element_to_be_clickable((By.XPATH, "//span[@class='el-radio__label']/span[contains(text(), '%s')]"%sold_leased))).click()
 click_next = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "btn-next")))
 
-#put element to lst
-Date=Address=Price=Changes=Saleable_Area=[]
-def it(x):
-    lst=[]
-    for i in x:
-        lst.append(i.text)
-    return lst
+def extract_data(element):
+    columns = element.find_elements(By.CSS_SELECTOR,"div[class*='bx--structured-list-td']")
+    Date = columns[0].text
+    Dev = columns[1].text
+    Price = columns[3].text
+    RiseBox = columns[4].text
+    Area = columns[5].text
+    return{
+       "Date": Date,
+       "Development": Dev,
+       "Consideration": Price,
+       "Change": RiseBox,
+       "Area": Area
+         }
 
-#find elements in web page
+data = []
+
 while True:
-    content = driver.find_element(By.CSS_SELECTOR, "#__layout .bx--structured-list")
-    info_date = content.find_elements(By.CLASS_NAME,"info-date")
-    Date = Date + it(info_date)
-    info_address = content.find_elements(By.XPATH, "//div[@class='cv-structured-list-data bx--structured-list-td']/div[contains(text(), '%s')]"%propertys)
-    Address = Address + it(info_address)
-    tranprice = content.find_elements(By.CLASS_NAME,"tranPrice")
-    Price = Price + it(tranprice)
-    info_changes = content.find_elements(By.CLASS_NAME,"riseBox")
-    Changes = Changes + it(info_changes)
-    feet = content.find_elements(By.XPATH, "//div[@class='cv-structured-list-data bx--structured-list-td']/div[contains(text(), 'ft²')]")
-    Saleable_Area = Saleable_Area + it(feet)
+    time.sleep(2)
+    contents = driver.find_element(By.CSS_SELECTOR,"div[class*='bx--structured-list-tbody']")
+    properties = contents.find_elements(By.CSS_SELECTOR,"div[class*='bx--structured-list-row']")
+    for propert in properties:
+        extracted_data = extract_data(propert)
+        data.append(extracted_data)
     if click_next.is_enabled():
         click_next.click()
     else:
         break #break the while loop if click next is disable
-    sleep(2)
+    time.sleep(2)
 driver.quit()
-    
-    
-#put data to csv
-df = pd.DataFrame()
-df['Date'] = Date
-df['Address'] = Address
-df['Price'] = Price
-df['Changes%'] = Changes
-df['Saleable_Area'] = Saleable_Area
+df = pd.DataFrame(data)
 df.to_csv("result.csv", index=False)
-
+print(df)
